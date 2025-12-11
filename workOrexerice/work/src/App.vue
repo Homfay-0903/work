791e5cfd3795394a4e553ecfaba52239fee3ed76
Homfay <2790165595@qq.com>
2025-12-11 17:16:09 +0800

    Ver-1.1

--- workOrexerice/work/src/App.vue
+++ workOrexerice/work/src/App.vue
@@ -4,10 +4,11 @@ import phy from './components/phy.vue';
 import phy2 from './components/phy2.vue';
 import phy3 from './components/phy3.vue'; //不好用
 import phy4 from './components/phy4.vue';
+import phy5 from './components/phy5.vue';
 </script>
 
 <template>
-  <phy2 />
+  <phy5 />
 </template>
 
 <style scoped></style>
