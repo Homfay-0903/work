e2c9f4cd7e037df5da1b9de391deebcceb127a85
Homfay <2790165595@qq.com>
2025-12-12 16:41:40 +0800

    Ver-1.8

--- workOrexerice/work/src/App.vue
+++ workOrexerice/work/src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import phy5 from './components/phy-fix/phy5.vue';
-import fsm1 from './components/fsm/fsm1.vue';
 import fsm3 from './components/fsm/fsm3.vue';
 import TrainingView from './components/muscle/TrainingView.vue';
+
+import cicrle from './components/quesOrexe/cicrle.vue';
 </script>
 
 <template>
-  <fsm3 />
+    <cicrle />
 </template>
 
-<style scoped></style>
+<style scoped>
+</style>
---
+++ workOrexerice/work/src/components/quesOrexe/cicrle.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="box">
+        <div class="legend-icon green-line">
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="less" scoped>
+    
+
+    .legend-icon {
+        width: 100px;
+        height: 100px;
+        border-radius: 50%;
+        border: 2px solid;
+        position: relative;
+
+        &.green-line {
+            background: green;
+        }
+
+        &::before {
+            content: '';
+            position: absolute;
+            top: 50%;
+            left: -8px;
+            right: -8px;
+            height: 2px;
+            background: red; // 继承border颜色
+            transform: translateY(-50%);
+            z-index: 0;
+          }
+
+        &::after {
+            content: ''; // 遮挡中间让其变成空心圆
+            position: absolute;
+            inset: 0;
+            background: #fff;
+            border-radius: 50%;
+            z-index: 1;
+          }
+    }
+</style>
