b49cc3ee6079bbd7431972cd18babe2d5b8fed0f
Homfay <2790165595@qq.com>
2026-01-14 17:16:39 +0800

    Ver-6.6

--- workOrexerice/work/src/App.vue
+++ workOrexerice/work/src/App.vue
@@ -4,7 +4,7 @@ import phy4 from './components/phy-fix/phy4.vue';
 //import phy-fix from './components/phy-fix/phy-fix.vue';
 
 import fsm3 from './components/fsm/fsm3.vue';
-import TrainingView from './components/muscle/TrainingView.vue';
+//import TrainingView from './components/muscle/TrainingView.vue';
 
 import cicrle from './components/quesOrexe/cicrle.vue';
 </script>
