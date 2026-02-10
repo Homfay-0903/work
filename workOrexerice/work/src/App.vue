f41b49470c179a0f6d6dee54b2c81b9730dbcf61
Homfay <2790165595@qq.com>
2026-02-10 14:37:57 +0800

    12.7

--- workOrexerice/work/src/App.vue
+++ workOrexerice/work/src/App.vue
@@ -1,21 +1,39 @@
 <script setup lang="ts">
-import phy5 from './components/phy-fix/phy5.vue';
-import phy4 from './components/phy-fix/phy4.vue';
+import phy5 from "./components/phy-fix/phy5.vue";
+import phy4 from "./components/phy-fix/phy4.vue";
 //import phy-fix from './components/phy-fix/phy-fix.vue';
 
-import fsm3 from './components/fsm/fsm3.vue';
+import fsm3 from "./components/fsm/fsm3.vue";
 //import TrainingView from './components/muscle/TrainingView.vue';
 
-import cicrle from './components/quesOrexe/cicrle.vue';
-import TodoItem from './components/exercise/index.vue';
+import cicrle from "./components/quesOrexe/cicrle.vue";
+import TodoItem from "./components/exercise/index.vue";
 
-const todoList = ref[{
-    
-}]
+interface ListItem {
+  id: number;
+  title: string;
+}
+const todoList = ref<ListItem[]>([
+  {
+    id: 1,
+    title: "Do the dishes",
+  },
+  {
+    id: 2,
+    title: "Take out the trash",
+  },
+  {
+    id: 3,
+    title: "Mow the lawn",
+  },
+]);
+
+
 </script>
 
 <template>
+    <span>Add a todo:</span>
+    <input type="text" v-model="aboutText" placeholder="....">
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
