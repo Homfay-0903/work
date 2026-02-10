ca1d3e0a9a02cd30b588b4f6d1f34b55179b00a4
Homfay <2790165595@qq.com>
2026-02-10 14:27:32 +0800

    12.6

--- workOrexerice/work/src/App.vue
+++ workOrexerice/work/src/App.vue
@@ -7,10 +7,14 @@ import fsm3 from './components/fsm/fsm3.vue';
 //import TrainingView from './components/muscle/TrainingView.vue';
 
 import cicrle from './components/quesOrexe/cicrle.vue';
+import TodoItem from './components/exercise/index.vue';
+
+const todoList = ref[{
+    
+}]
 </script>
 
 <template>
-    <phy5 />
 </template>
 
 <style scoped>
---
+++ workOrexerice/work/src/components/exercise/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <p>{{ prop.index }}: {{ prop.desc }}</p>
+    <button @click="$emit('remove')">Remove</button>
+</template>
+
+<script setup lang="ts">
+    interface Prop {
+        index: number,
+        desc: string
+    }
+    interface Emit {
+        "remove": []
+    }
+
+    const prop = defineProps<Prop>()
+    const emit = defineEmits<Emit>()
+</script>
